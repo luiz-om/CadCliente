8ce199d17076b9e63d1c59ea5ac9ef1fd15e6360
Luiz-OM <luizfernando_martins@hotmail.com>
2023-02-04 07:03:48 -0400

    Validao de campos vazio com for e foreach

--- cadCliente.js
+++ cadCliente.js
@@ -4,11 +4,64 @@ let pegaCampo = (id) => {
     return document.getElementById(id)
 }
 let salvar = () => {
+
+    let nameInput = pegaCampo('name').value
+    let telefoneInput = pegaCampo('telefone').value
+    let cpfInput = pegaCampo('cpf').value
     id++
+    //  let campos = [nameInput, telefoneInput]
+
+    /*     for (let index = 0; index < campos.length; index++) {
+            const element = campos[index];
+            if (!element) {
+                alert(`Preencha o campo ${element.label}`)
+                console.log(element)
+                return
+            }
+            
+        } */
+    const nameField = {
+        value: nameInput,
+        label: 'nome'
+    }
+    const telefoneField = {
+        value: telefoneInput,
+        label: 'telefone'
+    }
+    const cpfField = {
+        value: cpfInput,
+        label: 'cpf'
+    }
+    let campos = [nameField, telefoneField, cpfField]
+    campos.forEach((field) => {
+        //console.log(field);
+        const { value } = field
+        if (!value) {
+
+
+            alert(`Preencha o campo ${field.label}`)
+            console.log(field)
+            return
+        }
+    })
+
+    for (let index = 0; index < campos.length; index++) {
+        const element = campos[index];
+
+        const { value } = element
+        if (!value) {
+            alert(`Preencha o campo ${element.label}`)
+            console.log(element)
+            return
+        }
+
+    }
+
     const clientes = {
         id: id,
-        name: 'nameInput',
-        telefone: 'telefoneInput'
+        name: nameInput,
+        cpf: cpfInput,
+        telefone: telefoneInput
     }
     let campo = pegaCampo('res')
     campo.innerHTML = ''
@@ -16,14 +69,16 @@ let salvar = () => {
     CLIENTES.push(clientes)
 
     for (let index = 0; index < CLIENTES.length; index++) {
-
+        debugger
+        //intera array para exibir dados na tela
         const Cliente = CLIENTES[index];
-        console.log(Cliente);
-        campo.innerHTML += `<div>Nome: ${Cliente.name} - Telefone:
-         ${Cliente.telefone} <input id='btnexcluir' type="button" value='Excluir'> </div>`
-
+        //  console.log(Cliente);
+        campo.innerHTML += `<div id='telaDados'>Nome: ${Cliente.name} - Telefone:
+                 ${Cliente.telefone} <input id='btnexcluir' type="button" value='Excluir'> </div>`
     }
+
+
+
+
+
 }
-
-
-90
